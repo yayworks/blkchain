237acaf12a8e0a83abcbde11a66d5fa52c4829ab
yayworks <raj@yayworks.com>
2017-09-08 15:47:29 -0500

    Update jpy_lab_start.sh

--- jpy_lab_start.sh
+++ jpy_lab_start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-export PYTHONPATH=/root/anaconda3/envs/tensorflow/lib/python3.6:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/prettytensor-0.7.2-py3.6.egg:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/enum34-1.1.6-py3.6.egg:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/setuptools-27.2.0-py3.6.egg:/root/anaconda3/envs/tensorflow/lib/python3.6/site-packages/matplotlib:
-source /root/anaconda3/bin/activate tensorflow
+export PYTHONPATH=/usr/local/anaconda3/envs/tensorflow/lib/python3.6:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/prettytensor-0.7.2-py3.6.egg:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/enum34-1.1.6-py3.6.egg:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/setuptools-27.2.0-py3.6.egg:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/matplotlib:
+source /usr/local/anaconda3/bin/activate tensorflow
 
 app_name=$1
 port=$2
 dir=$3
 
 token=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 48 | head -n 1`
-echo $token
-/root/anaconda3/bin/jupyter  lab --ip=0.0.0.0 --port=$port --notebook-dir=$dir --allow-root --NotebookApp.token=$token  &
 
-term="Terminal"
+/usr/local/anaconda3/bin/jupyter lab --ip=0.0.0.0 --port=8888 --LabApp.token=$token &
+
+term="JupyterLab"
 if [ "$app_name" = "$term" ]
 then
 echo $app_name
