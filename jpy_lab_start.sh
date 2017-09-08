3918dd2393b1250aec17bb7a7711a34eb55bd9f5
yayworks <raj@yayworks.com>
2017-09-08 16:02:27 -0500

    Update jpy_lab_start.sh

--- jpy_lab_start.sh
+++ jpy_lab_start.sh
@@ -3,29 +3,18 @@
 export PYTHONPATH=/usr/local/anaconda3/envs/tensorflow/lib/python3.6:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/prettytensor-0.7.2-py3.6.egg:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/enum34-1.1.6-py3.6.egg:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/setuptools-27.2.0-py3.6.egg:/usr/local/anaconda3/envs/tensorflow/lib/python3.6/site-packages/matplotlib:
 source /usr/local/anaconda3/bin/activate tensorflow
 
-app_name=$1
-port=$2
-dir=$3
+app_name="JupyterLab"
+port=$1
+dir=$2
 
 token=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 48 | head -n 1`
 
 /usr/local/anaconda3/bin/jupyter lab --ip=0.0.0.0 --port=8888 --LabApp.token=$token &
 
-term="JupyterLab"
-if [ "$app_name" = "$term" ]
-then
-echo $app_name
+        out_str=${port}/?token=${token}
 
-        out_str=${app_name}:${port}/terminals/tree?token=${token}
 
-else
-
-        out_str=${app_name}:${port}/tree?token=${token}
-fi
-
-echo $out_str
-
-sed -i -e  "/${app_name}/d" /home/nimbix/tokens.list
+#sed -i -e  "/${app_name}/d" /home/nimbix/tokens.list
 
 echo ${out_str} >> /home/nimbix/tokens.list
 ~                                                                                
